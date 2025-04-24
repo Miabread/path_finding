716cac93405a60aad9669602bd861d78c01fd047
Miabread <miabreadloaf@gmail.com>
2025-04-24 15:20:41 -0400

    Add AStar

--- src/pathfinder.rs
+++ src/pathfinder.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 use bevy_ecs_tilemap::tiles::{TilePos, TileStorage};
 use rand::{rng, seq::IteratorRandom};
 use std::{
+    cmp::Reverse,
     collections::{BinaryHeap, HashSet, VecDeque},
     ops::ControlFlow,
 };
@@ -173,25 +174,105 @@ impl Algorithm for BreadthFirst {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+struct AStarTilePos {
+    pos: TilePos,
+    distance: u32,
+}
+
+impl AStarTilePos {
+    fn new(pos: TilePos, goals: &HashSet<TilePos>) -> Self {
+        let distance = goals
+            .iter()
+            .map(|goal| {
+                ((pos.x as i32 - goal.x as i32).pow(2) + (pos.y as i32 - goal.y as i32).pow(2))
+                    .isqrt()
+                    .abs() as u32
+            })
+            .min()
+            .unwrap_or(0);
+
+        AStarTilePos { pos, distance }
+    }
+}
+
+impl PartialEq for AStarTilePos {
+    fn eq(&self, other: &Self) -> bool {
+        self.distance == other.distance
+    }
+}
+
+impl Eq for AStarTilePos {}
+
+impl PartialOrd for AStarTilePos {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Reverse(self.distance).partial_cmp(&Reverse(other.distance))
+    }
+}
+
+impl Ord for AStarTilePos {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        Reverse(self.distance).cmp(&Reverse(other.distance))
+    }
+}
+
 #[derive(Debug, Default)]
 struct AStar {
-    queue: BinaryHeap<TilePos>,
+    queue: BinaryHeap<AStarTilePos>,
     visited: HashSet<TilePos>,
 }
 
 impl Algorithm for AStar {
     fn start(&mut self, start: TilePos) {
-        self.queue.push(start);
+        self.queue.push(AStarTilePos {
+            pos: start,
+            distance: 0,
+        });
         self.visited.insert(start);
     }
 
     fn step(
         &mut self,
-        _goals: &HashSet<TilePos>,
-        _storage: &TileStorage,
-        _tiles: Query<&mut TileState>,
+        goals: &HashSet<TilePos>,
+        storage: &TileStorage,
+        mut tiles: Query<&mut TileState>,
     ) -> ControlFlow<()> {
-        todo!()
+        let Some(tile) = self.queue.pop() else {
+            return ControlFlow::Break(());
+        };
+
+        if goals.contains(&tile.pos) {
+            return ControlFlow::Break(());
+        }
+
+        for neighbor in neighbors(tile.pos) {
+            if self.visited.contains(&neighbor) {
+                continue;
+            }
+
+            self.visited.insert(neighbor);
+
+            let Some(entity) = storage.checked_get(&neighbor) else {
+                continue;
+            };
+
+            let mut neighbor_state = tiles.get_mut(entity).unwrap();
+
+            if *neighbor_state == TileState::Wall {
+                continue;
+            }
+
+            self.queue.push(AStarTilePos::new(neighbor, goals));
+
+            neighbor_state.change_from(TileState::Empty, TileState::Queued);
+        }
+
+        tiles
+            .get_mut(storage.checked_get(&tile.pos).unwrap())
+            .unwrap()
+            .change_from(TileState::Queued, TileState::Visited);
+
+        ControlFlow::Continue(())
     }
 }
 
